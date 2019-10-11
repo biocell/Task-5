1b9b8df481059d75fe5d4442ef6652df993626cc
TheresaNde <31648818+TheresaNde@users.noreply.github.com>
2019-10-11 09:35:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,136 @@
-
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pontus Drive SlackBot</title>
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Hind:600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4293d2f86e.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<header class="top-header" role="banner">
+    <div class="navbar ">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a  class="navbar-brand">Pontus <span>Drive</span></a>
+                <button type="button"  class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <h6>Menu</h6>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse offset" id="navbar">
+                <ul class="nav navbar-nav justify-content-end">
+                    <li class="nav-item"><a href="/" class="nav-link">Home</a> </li>
+                    <li class="nav-item"><a href="login.php" class="nav-link">Login</a> </li>
+                    <li class="nav-item"><a href="register.php" class="nav-link">Sign up</a> </li>
+                    <li class="nav-item"><a href="about.php" class="nav-link">About us</a> </li>
+                    <li class="nav-item"><a href="FAQ.php" class="nav-link">FAQ</a> </li>
+                    <li class="nav-item"><a href="howitworks.php" class="nav-link">How It Works</a> </li>
+                </ul>
+            </div>
+        </div>
+</header>
+<section class="hero">
+    <div class="row m-0">
+        <div class="col-lg-7">
+            <div class="hero-heading">
+                <h2 align= "center">A better way to organize and share data within slack</h2>
+                <br>
+                <p class="font-weight-bold" align="justify" style="color: #7e7e7e">        Pontus slack drive, is built to help you store your slack messages
+                    for productive collaborations. It automatically saves conversations to our drive and easy to access.Once you create, gather, and start manipulating the messages you have received or sent out, they can quickly become disorganised. Most times we struggle to find messages.<br>
+                     To save time and prevent errors later on, you could decide how you will name and structure the messages you receive which would help you to locate, identify and retrieve them  quickly and accurately, therefore making it easier to manage your conversations.<br>
+                      Pontus slack drive would enable you archived old messages, create an admin folders to store your templates, user guides, notes etc . It could also help you store your messages into groups via topic/ area/person/subject. Finally it recognises the importance of backing up your files and ensures that all your messages are backed up automatically.</p>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="img-block">
+                <img src="assets/images/bg-master.png" width="100%">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hero2">
+    <div class="container m-0">
+        <div class="title-header">
+            <h2 align="center"> Benefits of Pontus Drive</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="benefits-col" align="center">
+                    <div class="benefits-header">
+                        <img class="benefit-img" src="assets/images/backup_icon.png" />
+                        <h3>Back-up with ease</h3>
+                    </div>
+                    <div class="benefits-body">
+                        <p>Pontus slack drive, makes it super easy to store all your data with just a click. We provide you backup
+                            of all your conversations and important data on our server in record time.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="benefits-col">
+                        <div class="benefits-header" align="center">
+                           <img class="benefit-img" src="assets/images/storage-icon.jpg" />
+                            <h3>Large amount of storage</h3>
+                        </div>
+                        <div class="benefits-body"  align="center">
+                            <p>You don’t have to worry about losing important data and conversations,
+                                with Pontus slackbot drive users get free useful amount of data storage.</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="benefits-col">
+                    <div class="benefits-header" align="center">
+                       <img class="benefit-img" src="https://res.cloudinary.com/dzflnjyii/image/upload/v1570780809/privacy_cd129m.png" />
+                        <h3>Privacy Guaranteed</h3>
+                    </div>
+                    <div class="benefits-body"  align="center">
+                        <p>With Pontus slackbot drive, you don’t have to worry about your personal conversations because we provide a safe space
+                            for you to store all of your personal data.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="benefits-co">
+                        <div class="benefits-header" align="center">
+                           <img class="benefit-img" src="https://res.cloudinary.com/dzflnjyii/image/upload/v1570780789/multitabs_nkqcoi.jpg" />
+                            <h3>See your messages anywhere</h3>
+                        </div>
+                        <div class="benefits-body"  align="center">
+                            <p>Have access to your conversations anywhere, at any time and any day in the world from any smartphone, tablet or computer. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+    </div>
+</section>
+<section class="hero-footer">
+    <div class="container">
+           <div  align="center">
+               <a href="register.php" class="btn btn-block btn-primary"><i class="fab fa-slack"></i> Get Started</a>
+               <footer class="container-fluid text-center">
+                <div class="row">
+                        <h4>Contact Us</h4>
+                        <a href="mailto:hello@pontusbot.co?Subject=Hello%20there" target="_top"><h6>hello@pontusbot.co</h6></a>
+                 <a href="#"><h4 style="color: rgb(253, 253, 253);">Potus Drive</h4></a>
+                        <p>© 2019 Pontus NG. All rights reserved.</p>
+                
+                
+                
+                </div>
+            </footer>
+           
+            </div>
+    </div>
+</section>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
